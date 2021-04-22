fd835413f62840104b2b07a6e3232726d0af0de5
Simon Hildrew <simon@hildrew.net>
2021-04-22 16:58:55 +0100

    add more files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as pj from 'projen';
 
 export interface GuTsLambdaOptions extends pj.TypeScriptProjectOptions {
-  readonly stacks: string[];
-  readonly regions?: string[];
+  readonly stack: string;
+  readonly region?: string;
+  readonly runtime?: string;
 }
 
 export class GuTsLambda extends pj.TypeScriptAppProject {
@@ -35,8 +36,8 @@ export class GuTsLambda extends pj.TypeScriptAppProject {
 
     new pj.YamlFile(this, 'riff-raff.yaml', {
       obj: {
-        stacks: options.stacks,
-        regions: options.regions ?? ['eu-west-hackday'],
+        stacks: [options.stack],
+        regions: [options.region ?? 'eu-west-1'],
       },
       committed: true,
     });
@@ -53,5 +54,134 @@ export class GuTsLambda extends pj.TypeScriptAppProject {
         },
       },
     });
+
+    new pj.JsonFile(this, 'cdk/tsconfig.json', {
+      committed: true,
+      obj: {
+        'ts-node': {
+          compilerOptions: {
+            module: 'CommonJS',
+          },
+        },
+        'compilerOptions': {
+          target: 'ES2020',
+          module: 'ES2020',
+          moduleResolution: 'node',
+          lib: ['ES2020'],
+          declaration: true,
+          strict: true,
+          noImplicitAny: true,
+          strictNullChecks: true,
+          esModuleInterop: true,
+          noImplicitThis: true,
+          alwaysStrict: true,
+          noUnusedLocals: false,
+          noUnusedParameters: false,
+          noImplicitReturns: true,
+          noFallthroughCasesInSwitch: false,
+          inlineSourceMap: true,
+          inlineSources: true,
+          experimentalDecorators: true,
+          strictPropertyInitialization: false,
+          typeRoots: ['./node_modules/@types'],
+          outDir: 'dist',
+        },
+        'include': ['lib/**/*', 'bin/**/*'],
+        'exclude': [
+          'node_modules',
+          'cdk.out',
+          'lib/**/*.test.ts',
+          'lib/**/__snapshots__/**',
+        ],
+      },
+    });
+
+    new pj.JsonFile(this, 'cdk/package.json', {
+      committed: true,
+      obj: {
+        name: 'cdk',
+        version: '0.1.0',
+        bin: {
+          cdk: 'bin/cdk.js',
+        },
+        scripts: {
+          'build': 'tsc --noEmit',
+          'watch': 'tsc -w',
+          'test': 'jest --runInBand --detectOpenHandles',
+          'test:dev': 'jest --runInBand --detectOpenHandles --watch',
+          'format': 'prettier --write "{lib,bin}/**/*.ts"',
+          'cdk': 'cdk',
+          'lint': 'eslint lib/** bin/** --ext .ts --no-error-on-unmatched-pattern',
+          'generate': 'cdk synth --path-metadata false --version-reporting false',
+        },
+        devDependencies: {
+          '@aws-cdk/assert': '1.97.0',
+          '@guardian/eslint-config-typescript': '^0.5.0',
+          '@types/jest': '^26.0.22',
+          '@types/node': '14.14.41',
+          '@typescript-eslint/eslint-plugin': '^4.22.0',
+          '@typescript-eslint/parser': '^4.22.0',
+          'aws-cdk': '1.97.0',
+          'eslint': '^7.24.0',
+          'eslint-config-prettier': '^8.2.0',
+          'eslint-plugin-eslint-comments': '^3.2.0',
+          'eslint-plugin-import': '^2.22.1',
+          'eslint-plugin-prettier': '^3.4.0',
+          'jest': '^26.6.3',
+          'prettier': '^2.2.0',
+          'ts-jest': '^26.5.5',
+          'ts-node': '^9.0.0',
+          'typescript': '~4.2.4',
+        },
+        dependencies: {
+          '@aws-cdk/core': '1.97.0',
+          '@guardian/cdk': '12.0.0',
+          'source-map-support': '^0.5.16',
+        },
+      },
+    });
+
+    const { stack, name, runtime } = options;
+
+    new pj.TextFile(this, 'cdk/bin/cdk.ts', {
+      lines: `#!/usr/bin/env node
+import "source-map-support/register";
+import { App } from "@aws-cdk/core";
+import {GuStack} from "@guardian/cdk/lib/constructs/core";
+import {GuApiLambda} from "@guardian/cdk/lib/patterns/api-lambda";
+import {Runtime} from "@aws-cdk/aws-lambda";
+
+const app = new App();
+
+const stack = new GuStack(app, "${stack}-${name}", {
+    stack: "${stack}",
+});
+
+const api = new GuApiLambda(stack, "${stack}-${name}-api", {
+    fileName: "${name}.zip",
+    handler: "index.handler",
+    runtime: new Runtime("${ runtime ?? 'NODEJS_14_X'}"),
+    monitoringConfiguration: {noMonitoring: true},
+    app: "${name}",
+    apis: [{
+        id: "api"
+    }],
+});
+      `.split('\n'),
+    });
+
+    new pj.TextFile(this, 'cdk/script/ci', {
+      lines: `#!/usr/bin/env bash
+set -e
+
+npm install -g yarn
+
+yarn install --frozen-lockfile
+yarn lint
+yarn build
+yarn test
+yarn generate`.split('\n'),
+    });
+
   }
 }
