f7c217df47773d7b7acc64848ac8e2488a013f43
Simon Hildrew <simon@hildrew.net>
2021-04-22 11:09:02 +0100

    Add specific node version for workflow

--- .projenrc.js
+++ .projenrc.js
@@ -74,7 +74,7 @@ const project = new TypeScriptProject({
   // releaseWorkflowSetupSteps: undefined,                                     /* A set of workflow steps to execute in order to setup the workflow container. */
   // workflowBootstrapSteps: 'yarn install --frozen-lockfile && yarn projen',  /* Workflow steps to use in order to bootstrap this repo. */
   // workflowContainerImage: undefined,                                        /* Container image to use for GitHub workflows. */
-  // workflowNodeVersion: undefined,                                           /* The node version to use in GitHub workflows. */
+  workflowNodeVersion: "14",                                           /* The node version to use in GitHub workflows. */
 
   /* ProjectOptions */
   // clobber: true,                                                            /* Add a `clobber` task which resets the repo to origin. */
