bf179205e5fdb1e84e4e0a9d405a8185ef7f4fe1
Akash Askoolum <akash1810@gmail.com>
2021-04-23 11:02:54 +0100

    fix env vars in GHA workflow

--- src/index.ts
+++ src/index.ts
@@ -233,10 +233,10 @@ yarn node-riffraff-artifact`.split('\n'),
           },
           {
             run: './script/ci',
-            env: [{
+            env: {
               AWS_ACCESS_KEY_ID: '${{ secrets.AWS_ACCESS_KEY_ID }}',
               AWS_SECRET_ACCESS_KEY: '${{ secrets.AWS_SECRET_ACCESS_KEY }}',
-            }],
+            },
           },
         ],
       },
